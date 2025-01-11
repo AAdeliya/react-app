91dd51fcd1a0a2cb5e6f8939a940ced0797214a4
firstPush <adel.sdet2@gmail.com>
2025-01-10 23:34:15 -0600

    first commit

---
+++ src/Message.tsx
@@ -0,0 +1,11 @@
+//PascalCasing
+function Message() {
+    //JSX: JavaScript XML
+    const name  = 'Hello Adel';
+    if (name) 
+       return <h1>  {name}</h1>;
+    return <h1>Hello World</h1>;
+
+}
+
+export default Message;
